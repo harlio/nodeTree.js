cd1896b0ba8e3281419cf575bbdd81731ce78c66
Harley Jessop <harley@neutroninteractive.com>
2014-04-04 10:29:27 -0600

    added more stable plugin wrapper and structure

--- checkboxtree.js
+++ checkboxtree.js
@@ -1,121 +1,185 @@
 /**
-** checkboxtree 1.2
+** checkboxtree 1.3
 ** No copyrights or licenses.
 ** @author:     @harleyjessop
 ** @repository: https://github.com/harleyjessop/checkboxtree.js
 **
 **/
 
-(function($) {
-    $.fn.checkboxtree = function(options){
-        // default configuration properties
-        var defaults = {
-            container:      this,
-            tree:           '.tree', // Wrapper of the entire nested list.
-            node:           '.node', // Wrapper class given to each node in the tree	
-            nodeBtnLabel:   '.node label', // Active area on each node to display or hide nested nodes.
-            nodeDepth:      '.greatgrandchild' // Default dept of tree lists		
-        };
+;(function ( $, window, document, undefined ) {
 
-        var options = $.extend(defaults, options);
+		// Create the defaults once
+		var pluginName = "checkboxtree",
+				defaults = {
+                tree:           '.tree', // Wrapper of the entire nested list.
+                node:           '.node', // Wrapper class given to each node in the tree	
+                nodeBtnLabel:   '.node label', // Active area on each node to display or hide nested nodes.
+                nodeDepth:      '.greatgrandchild' // Default dept of tree lists		
+		};
 
-        $(options.node, options.tree).click(function() {
-            if (!$(this).is(options.nodeDepth)){
-                $(this).parent().toggleClass('active');
-                _toggleIcon($(this));
-            }
-        });
+		// The actual plugin constructor
+		function Plugin ( element, options ) {
+				this.element = element;
+				this.settings = $.extend( {}, defaults, options );
+				this._defaults = defaults;
+				this._name = pluginName;
+				this.init();
+		}
 
-        options.container.on('click', '.check-all-nodes', function() {
-            $(options.tree + ' :checkbox').prop({checked: 'checked', indeterminate: false }).trigger('change');
-            colorCoded();
-        }).on('click', '.uncheck-all-nodes', function() {
-            $(options.tree + ' :checkbox').removeAttr('checked').trigger('change');
-            colorCoded();
-        }).on('click', '.expand-all-nodes', function() {
-            $(options.node, options.tree).each(function(){
-                $(this).parent().addClass('active');
-                $(options.nodeDepth).parent().removeClass('active');
-                _toggleIcon($(this));
-            });
-        }).on('click', '.collapse-all-nodes', function() {
-            $(options.node, options.tree).each(function(){
-                $(this).parent().removeClass('active');
-                _toggleIcon($(this));
-            });
-        });
+		Plugin.prototype = {
+				init: function () {
+					// Place initialization logic here
+					// You already have access to the DOM element and
+					// the options via the instance, e.g. this.element
+					// and this.settings
+					// you can add more functions like the one below and
+					// call them like so: this.yourOtherFunction(this.element, this.settings).
+                    var self = this,
+                        container = this.element;
 
-        // Make checkbox label not mess with the toggle function
-        $(options.nodeBtnLabel).on('click', function(e) {
-            e.stopPropagation();
-        });
+                    $(self.settings.node, self.settings.tree).click(function() {
 
-        $(':checkbox', options.tree).on('change', function(e) {
-            e.stopPropagation();
-            var checked = $(this).prop("checked");
-            var nodeContainer = $(this.parentNode.parentNode.parentNode);
-            var descendantNodes = $(':checkbox', nodeContainer).not($('.added :checked'));
-            descendantNodes.prop({
-                indeterminate: false,
-                checked: checked
-            });
-            colorCoded(descendantNodes);
-            if (!nodeContainer.parent('#parent-list').length){
-                checkSiblings(nodeContainer, checked);
-            }
-            colorCoded(this);
-        });
+                        if (!$(this).is(self.settings.nodeDepth)) {
+                            $(this).parent().toggleClass('active');
+                            self.toggleIcon($(this));
+                        }
+                    });
+            
+                    $(container).on('click', '.check-all-nodes', function() {
 
-        // Handles the color coding of the tables. Green is checked, yellow is indeterminate.
-        function colorCoded(n){
-            $(n).each(function(){
-                $(this).closest(options.node)
-                .toggleClass('checked', $(n).is(':checked'))
-                .toggleClass('indeterminate', $(n).prop("indeterminate") === true);
-            });
-        }
+                        $(self.settings.tree + ' :checkbox')
+                            .prop({checked: 'checked', indeterminate: false })
+                            .trigger('change');
+                        self.colorCoded();
 
-        function _toggleIcon(n) {
-            // Change the icon.
-            if (n.parent().hasClass('active')) {
-                // The user wants to collapse the child list.
-                n.find('i').removeClass('icon-plus-sign').addClass('icon-minus-sign');
-            } else {
-                // The user wants to expand the child list.
-                n.find('i').removeClass('icon-minus-sign').addClass('icon-plus-sign');
-            }
-        }
+                    }).on('click', '.uncheck-all-nodes', function() {
 
-        // checks all the siblings, parents, and parents siblings to determine the appropriate checked state for its parents.
-        function checkSiblings(el, checked) {
-            if (!el.length) return; 
-            var parent = el.parent().parent();
-            var all = true;
-            var thisNode = parent.children(options.node).find(':checkbox');
-            el.siblings().not('.empty').each(function() {
-                return all = ($(this).find(':checkbox').prop("checked") === checked);
-            });
-            if (all && checked) {
-                $(':checkbox', parent).prop({
-                    indeterminate: false,
-                    checked: checked
-                });
-                colorCoded(thisNode);
-                checkSiblings(parent, checked);
-            } else if (all && !checked) {
-                thisNode.prop("checked", checked);
-                thisNode.prop("indeterminate", (parent.find(':checked').length));
-                colorCoded(thisNode);
-                checkSiblings(parent, checked);
-            } else {
-                el.parents("li").children(options.node).find(':checkbox').prop({
-                    indeterminate: true,
-                    checked: false
-                });
-                colorCoded(el.parents("li").children(options.node).find(':checkbox'));
-            }
-        }
-        checkSiblings($(options.nodeDepth + ' :checked').closest('li'), true);
-    };
+                        $(self.settings.tree + ' :checkbox')
+                            .removeAttr('checked')
+                            .trigger('change');
+                        self.colorCoded();
 
-})(jQuery);
+                    }).on('click', '.expand-all-nodes', function() {
+
+                        $(self.settings.node, self.settings.tree).each(function(){
+                            $(this).parent().addClass('active');
+                            $(self.settings.nodeDepth).parent().removeClass('active');
+                            self.toggleIcon($(this));
+                        });
+
+                    }).on('click', '.collapse-all-nodes', function() {
+
+                        $(self.settings.node, self.settings.tree).each(function(){
+                            $(this).parent().removeClass('active');
+                            self.toggleIcon($(this));
+                        });
+
+                    });
+            
+                    // Make checkbox label not mess with the toggle function
+                    $(self.settings.nodeBtnLabel).on('click', function(e) {
+                        e.stopPropagation();
+                    });
+            
+                    $(':checkbox', self.settings.tree).on('change', function(e) {
+
+                        e.stopPropagation();
+                        var checked = $(this).prop("checked");
+                        var nodeContainer = $(this.parentNode.parentNode.parentNode);
+                        var descendantNodes = $(':checkbox', nodeContainer).not($('.added :checked'));
+
+                        descendantNodes.prop({
+                            indeterminate: false,
+                            checked: checked
+                        });
+
+                        self.colorCoded(descendantNodes);
+                        if (!nodeContainer.parent('.parent-list').length){
+                            self.checkSiblings(nodeContainer, checked);
+                        }
+                        self.colorCoded(this);
+
+                    });
+
+                    self.checkSiblings($(self.settings.nodeDepth + ' :checked').closest('li'), true);
+
+				},
+
+				colorCoded: function (n) {
+
+                    var self = this;
+                    $(n).each(function(){
+                        $(this).closest(self.settings.node)
+                            .toggleClass('checked', $(n).is(':checked'))
+                            .toggleClass('indeterminate', $(n).prop("indeterminate") === true);
+                    });
+				},
+
+                toggleIcon: function (n) {
+                    // Change the icon.
+                    if (n.parent().hasClass('active')) {
+                        // The user wants to collapse the child list.
+                        n.find('i').removeClass('icon-plus-sign').addClass('icon-minus-sign');
+                    } else {
+                        // The user wants to expand the child list.
+                        n.find('i').removeClass('icon-minus-sign').addClass('icon-plus-sign');
+                    }
+                },
+
+                checkSiblings: function (el, checked) {
+
+                    if (!el.length) return;
+
+                    var self = this,
+                        parent = el.parent().parent(),
+                        all = true;
+
+                    var thisNode = parent.children(self.settings.node).find(':checkbox');
+
+                    el.siblings().not('.empty').each(function() {
+                        return all = ($(this).find(':checkbox').prop("checked") === checked);
+                    });
+
+                    if (all && checked) {
+
+                        $(':checkbox', parent).prop({
+                            indeterminate: false,
+                            checked: checked
+                        });
+                        self.colorCoded(thisNode);
+                        self.checkSiblings(parent, checked);
+
+                    } else if (all && !checked) {
+
+                        thisNode.prop("checked", checked);
+                        thisNode.prop("indeterminate", (parent.find(':checked').length));
+                        self.colorCoded(thisNode);
+                        self.checkSiblings(parent, checked);
+
+                    } else {
+
+                        el.parents("li").children(self.settings.node).find(':checkbox').prop({
+                            indeterminate: true,
+                            checked: false
+                        });
+                        self.colorCoded(el.parents("li").children(self.settings.node).find(':checkbox'));
+
+                    }
+                }
+		};
+
+		// A really lightweight plugin wrapper around the constructor,
+		// preventing against multiple instantiations
+		$.fn[ pluginName ] = function ( options ) {
+				this.each(function() {
+						if ( !$.data( this, "plugin_" + pluginName ) ) {
+								$.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
+						}
+				});
+
+				// chain jQuery functions
+				return this;
+		};
+
+})( jQuery, window, document );
+
